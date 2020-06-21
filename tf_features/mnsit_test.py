ce097840896078d491ddc61f5abe04f74535fb69
TuringEmmy <yonglonggeng@163.com>
2020-06-21 08:02:46 +0800

    delete idea

---
+++ tf_features/mnsit_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2020-06-21-07:54
+# @Author   : TuringEmmy
+# @Email    : yonglonggeng@163.com
+# @WeChat   : csy_lgy
+# @File     : mnsit_test.py
+# @Project  : PaperProject
---
+++ tf_features/tf_save_csv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2020-06-20-23:26
+# @Author   : TuringEmmy
+# @Email    : yonglonggeng@163.com
+# @WeChat   : csy_lgy
+# @File     : tf_save_csv.py
+# @Project  : PaperProject
